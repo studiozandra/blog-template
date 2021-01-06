d130161432bd2e6769cdcb5d7702a2db6fcd51f6
studiozandra <zan@studiozandra.com>
2021-01-05 23:21:25 -1000

    split about and tech pages into template and content sections

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-cat templates/top_about.html content/index.html templates/bottom_about.html > docs/about.html
+cat templates/top_about.html content/about.html templates/bottom_about.html > docs/about.html
+cat templates/top_technology.html content/technology.html templates/bottom.html > docs/about.html
