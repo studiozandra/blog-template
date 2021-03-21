ad35da17f3bfcff4e326f85024d21c05b6dfa2f3
zan <zan@studiozandra.com>
2021-03-20 15:10:34 -1000

    add content_sandwich func

--- build.py
+++ build.py
@@ -27,29 +27,39 @@ pages = [
 ]
 # loop through this list and pull out differnt things at diff times
 
+def content_sandwich(name_of_page, output_page):
+
+    # Read in the base template header and footer page elements
+    base = open("templates/base.html").read()
+
+    content = open(name_of_page).read()
+
+    # Use the string replace 
+    full_page = base.replace("{{content}}", content)
+    
+    print("writing file", name_of_page)
+    return open(output_page, "w+").write(full_page)
+
+
 
 def main():
     # main function
     print("Hello, your static site generator is runnin'")
 
 
-    # Read in the base template header and footer page elements
-    base = open("templates/base.html").read()
-
-    
-
     # Get the page elements from the new list using a loop
     for page in pages:
 
         print('Gettin\'', page["title"], 'file...', page["filename"], '...')
        
-        content = open(page["filename"]).read()
+        content = page["filename"]
 
-        # Use the string replace 
-        full_page = base.replace("{{content}}", content)
+        # # Use the string replace 
+        # full_page = base.replace("{{content}}", content)
         
-        print("writing file", page["output"])
-        open(page["output"], "w+").write(full_page)
+        # print("writing file", page["output"])
+        # open(page["output"], "w+").write(full_page)
+        content_sandwich(content, page["output"])
         
 
 
