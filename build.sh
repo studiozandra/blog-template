b195e4404d8e4c3662109bb9455974b2677714f7
zan <zan@studiozandra.com>
2021-03-20 22:22:54 -1000

    delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-cat templates/top.html content/index.html templates/bottom.html > docs/index.html
-cat templates/top_about.html content/about.html templates/bottom.html > docs/about.html
-cat templates/top_technology.html content/technology.html templates/bottom.html > docs/technology.html
-cat templates/top_design.html content/design.html templates/bottom_design.html > docs/design.html
-cat templates/top_contact.html content/contact.html templates/bottom.html > docs/contact.html
