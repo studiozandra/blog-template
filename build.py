e787a7bab69bc5d8c925fcd0a6cabb88f2bb84f3
zan <zan@studiozandra.com>
2021-03-24 22:47:30 -1000

    add active link highlighting

--- build.py
+++ build.py
@@ -72,7 +72,7 @@ def copyright_year(entitled_base):
 # generate nav links automatically 
 # if param === pages[filename], + inline styling of pages[title], else + pages[title]
 
-def auto_links():
+def auto_links(current_page_title):
 
     # Read in the base template header and footer page elements
     base = open("templates/base.html").read()
@@ -82,7 +82,10 @@ def auto_links():
     for page in pages:
         nav_links_html = nav_links_html + '<a class="p-2 text-muted" href="'
         nav_links_html += page['filename'].replace("content/", "", 1)
-        nav_links_html += '">'
+        nav_links_html += '"'
+        if current_page_title == page["title"]:
+            nav_links_html += 'style="text-decoration: none;background-color: #4a4864;"'
+        nav_links_html += '>'
         nav_links_html += page['title']
         nav_links_html += '</a>'
         nav_added_base = base.replace('{{navlinks}}', nav_links_html)
@@ -105,7 +108,7 @@ def main():
         print('Getting', title, 'file...', content, '...')
 
         # add navbar links
-        nav_added_base = auto_links()
+        nav_added_base = auto_links(title)
         
         # call the function to write in the page title
         entitled_base = entitle_base(title, nav_added_base)
